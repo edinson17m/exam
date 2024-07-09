262d072bbd08be69b64f260293ee5d085fa22076
edinson17m <167745353+edinson17m@users.noreply.github.com>
2024-07-09 11:39:06 +0300

    Stayled_component (#1)

---
+++ src/components/Menu/Menu.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const MenuWrapper = styled.div`
+    width: 95%;
+    height: 100px;
+    background-color: blue;
+    color: aqua;
+`;
+
+export const DisplyCentr = styled.div`
+     display: flex;
+     flex-direction: row;
+     align-items: center;
+     justify-content: center;
+
+
+`;
